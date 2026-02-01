6ef3bab54f0ad891c4890bfa49efd31173d347c7
navi705 <omlain20@gmail.com>
2026-02-01 16:24:58 +0100

    fix

--- parser/init-db.sh
+++ parser/init-db.sh
@@ -4,14 +4,27 @@ set -e
 # This script creates a second user and database using environment variables
 # provided via docker-compose/env file.
 
+# 1. Create user and grant DB permissions (connecting to system 'postgres' db)
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-    CREATE USER $PROJECT_USER WITH PASSWORD '$PROJECT_PASSWORD';
-    GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $PROJECT_USER;
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_user WHERE usename = '$PROJECT_USER') THEN
+            CREATE USER $PROJECT_USER WITH PASSWORD '$PROJECT_PASSWORD';
+        END IF;
+    END
+    \$\$;
+    GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO "$PROJECT_USER";
+EOSQL
+
+# 2. Grant schema permissions (connecting directly to our app database)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    -- Grant schema permissions
+    GRANT USAGE, CREATE ON SCHEMA public TO "$PROJECT_USER";
+    ALTER SCHEMA public OWNER TO "$PROJECT_USER";
     
-    -- Connect to the local database to grant schema permissions
-    \c $DB_NAME
-    
-    GRANT ALL ON SCHEMA public TO $PROJECT_USER;
-    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $PROJECT_USER;
-    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $PROJECT_USER;
+    -- Ensure future permissions
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "$PROJECT_USER";
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO "$PROJECT_USER";
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO "$PROJECT_USER";
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO "$PROJECT_USER";
 EOSQL
